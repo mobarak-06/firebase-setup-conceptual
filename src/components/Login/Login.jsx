2eb43f756d4d39746442ce4cfaeb9e54d259134d
Mobarak Hossain <mobarakhossain121838@gmail.com>
2024-06-28 23:12:26 +0600

    first commit

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+    return (
+        <div>
+            <h2>I am login</h2>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,9 @@
+const Register = () => {
+    return (
+        <div>
+           <h2> Iam register</h2>
+        </div>
+    );
+};
+
+export default Register;
